ce0d85557427e54f395acf67ba6e6fbf605a6a74
corentin desayes <corentin.desayes@epitech.eu>
2019-08-21 23:11:20 +0200

    remove services

--- services/disk_management/disk_service.py
+++
@@ -1,7 +0,0 @@
-class disk_service(object):
-
-    def get_disk_list():
-        return list(map(lambda x: Disk(x), parted.getAllDevices()))
-
-    def get_disk(device_path):
-        return Disk(device_path)
